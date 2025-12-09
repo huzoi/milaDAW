2ebabe76fd1bd8322d0778a829097ab5cfd290c3
hVSC <h@huzoi.me>
2025-12-09 21:12:19 +0100

    char2char

--- @/PEM/C/RA1/12-9/char2char/char2char.c
+++ @/PEM/C/RA1/12-9/char2char/char2char.c
@@ -1,3 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
+
+int es_vocal(char c){
+    c = tolower(c);
+    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+            c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú');
+}
+
+int main(){
+    FILE *pelis;
+    char caracter;
+    int total_vocals = 0;
+    
+    pelis = fopen("pelis.txt", "r");
+    
+    if (pelis == NULL){
+        printf("Error: No s'ha pogut obrir l'arxiu pelis.txt\n");
+        return 1;
+    }
+    
+    while ((caracter = fgetc(pelis)) != EOF){
+        if (es_vocal(caracter)){
+            total_vocals++;
+        }
+    }
+    
+    fclose(pelis);
+    
+    printf("L'arxiu 'pelis.txt' conté %d vocals.\n", total_vocals);
+    
+    return 0;
+}
---
+++ @/PEM/C/RA1/12-9/char2char/cp/cp.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(int argc, int *argv){
+    FILE cp_origen, cp_desti;
+}
+
