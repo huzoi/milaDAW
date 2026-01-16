c5b813de53373cdad5ed8e181970a95b77551447
hVSC <h@huzoi.me>
2026-01-16 17:28:07 +0100

    16-1v1

--- @/PEM/Java/RA1/1-14/ex8/NumerosFinsZeroV2.java
+++ @/PEM/Java/RA1/1-14/ex8/NumerosFinsZeroV2.java
@@ -5,6 +5,7 @@ public class NumerosFinsZeroV2 {
         int[] numeros = new int[100];
         int contador = 0;
         int numero;
+        boolean primerZero = false;
         
         do {
             String entrada = JOptionPane.showInputDialog("Introdueix un número (0 per acabar):");
@@ -16,10 +17,17 @@ public class NumerosFinsZeroV2 {
             }
         } while (numero != 0);
 
+        if (numero == 0 && contador == 0) {
+                primerZero = true;
+                JOptionPane.showMessageDialog(null, "El primer nombre ha sigut un 0");
+            }
+
+        if (numero == 0 && primerZero == false) {
         String ordreInvers = "NÚMEROS: \n\n";
         for (int i = contador - 1; i >= 0; i--) {
             ordreInvers += numeros[i] + "\n";
         }
         JOptionPane.showMessageDialog(null, ordreInvers);
+        }
     }
 }
