499f03282831b92b6b8e465349e5a2dfeea9e3f8
h <h@huzoi.me>
2026-01-16 23:52:25 +0100

    ex4

--- @/PEM/Java/RA1/1-16/fitxa2/ex4/ExtraureCadena.java
+++ @/PEM/Java/RA1/1-16/fitxa2/ex4/ExtraureCadena.java
@@ -2,9 +2,20 @@ import java.util.Scanner;
 
     public class ExtraureCadena {
         public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
             String s = "Posa els mitjons a la rentadora";
-        
-        
-        
+
+            System.out.println("Text Original: " + s);
+            System.out.println("Introdueix el numero de caracters des del final: ");
+            int numCaracters = sc.nextInt();
+
+            if (numCaracters > 0 && numCaracters <= text.length()) {
+                String subs = s.subs(s.length() - numCaracters);
+                System.out.println("Subcadena (Ultims " + numCaracters + "caracters): " + subs);
+            } else {
+                System.out.println("Numero de caracters no valid");
+            }
+
+            sc.close();
         }
     }
